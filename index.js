5facbf9d0135626e4e300be23d0bb41fa352cff9
Bibo <bibomain@gmail.com>
2016-12-12 22:42:12 -0800

    check env var

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,3 +13,15 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var firebaseUrl = 'https://sockettime.firebaseio.com/test.json?auth='+process.env.firebaseToken
+
+request.post(
+    firebaseUrl,
+    { json: { key: 'wtf' } },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    }
+);
